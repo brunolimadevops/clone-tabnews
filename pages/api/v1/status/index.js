fb65da37569fb7969effb6c83e2d1c60703ff474
Bruno Lima <blima1989@gmail.com>
2025-07-17 18:19:20 -0300

    Change status mesage

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,9 @@
 function status(request, response) {
   response
     .status(200)
-    .json({ resposta: "Alunos do curso.dev São pessoas acima da média!" });
+    .json({
+      Status: "Tudo funcionando perfeitamente. Bora tomar uma cerveja?",
+    });
 }
 
 export default status;
