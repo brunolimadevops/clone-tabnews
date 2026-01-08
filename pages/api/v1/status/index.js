56cc5a71033599b471e831097d25e551184d62e1
Bruno Lima <blima1989@gmail.com>
2026-01-07 22:47:23 -0300

    add database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,10 @@
-function status(request, response) {
-  response
-    .status(200)
-    .json({
-      Status: "Tudo funcionando perfeitamente. Bora tomar uma cerveja?",
-    });
+import database from "../../../../infra/database.js";
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as SUM;");
+  console.log(result.rows);
+  response.status(200).json({
+    Status: "Tudo funcionando perfeitamente. Bora tomar uma cerveja?",
+  });
 }
 
 export default status;
